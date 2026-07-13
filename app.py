8172f021aab3def13e70df5e7dfe07904b31bb90
auzzie347 <116705960+auzzie347@users.noreply.github.com>
2026-07-13 19:40:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def generate_ai_multi_evaluation(results_list):
     try:
         client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
         response = client.models.generate_content(
-            model='gemini-2.5-flash',
+            model="gemini-3.5-flash'"
             contents=prompt,
         )
         return response.text
@@ -201,7 +201,7 @@ def generate_ai_individual_feedback(res):
     try:
         client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
         response = client.models.generate_content(
-            model='gemini-2.5-flash',
+            model="gemini-3.5-flash",
             contents=prompt,
         )
         return response.text
